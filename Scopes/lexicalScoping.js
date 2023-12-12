2f850173939ac7dc01cfbd1b94b9d04c54f46540
vivekrajput-93 <viveksinghraajputt@gmail.com>
2023-12-12 19:39:15 +0530

    Scopes

---
+++ Scopes/lexicalScoping.js
@@ -0,0 +1,24 @@
+var teacher = "sanket"; // target
+
+function fun() {
+    var teacher = "Pulkit"; // target
+    console.log(teacher); // source
+}
+console.log(teacher) // source
+fun(); // source
+
+
+// Example of Auto-Global
+
+var teacher = "sanket";
+
+function fun() {
+    var teacher = "Pulkit";
+    teachingAssist = "vibhav"; // if there is no declaration then it consider as auto-global in execution phase.
+    console.log(teacher)
+    console.log(teachingAssist);
+}
+
+fun();
+console.log(teacher) // source
+console.log(teachingAssist) // source
